bfbc6821e5d726958e1105bd258c1901846ba242
GlitchYou <38540168+GlitchYou@users.noreply.github.com>
2020-11-26 20:23:46 -0300

    Update design.py

--- quick_tools/design.py
+++ quick_tools/design.py
@@ -3,33 +3,7 @@ Design
 
 Create custom inputs
 """
-
 from .stylors import *
-
-
-def lin(text='', sym='-', num=50, center=True):
-    from re import findall, sub
-
-    if text == '':
-        print(sym * num)
-
-    else:
-
-        colors = findall(r'\x1b\[(?:[\d;]+)?m', text)
-        csym = len(sub(r'\x1b\[(?:[\d;]+)?m', '', sym))
-
-        cnum = int(num * csym)
-
-        if len(colors) > 0:
-            for n in colors:
-                cnum += len(n)
-
-        if center:
-            text = text.center(cnum)
-
-        print(sym * num)
-        print(text)
-        print(sym * num)
 
 
 def menu(*label, prompt='\n> ', mask='{i}) {p}'):
@@ -52,21 +26,3 @@ def menu(*label, prompt='\n> ', mask='{i}) {p}'):
         except (ValueError, TypeError):
             print(f'{cr}Type an integer{nn}')
             wait(1)
-
-
-def read_int(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except (ValueError, TypeError):
-            print(f'{cr}Type an integer{nn}')
-
-
-def read_float(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except (ValueError, TypeError):
-            print(f"{cr}Enter a decimal number{nn}")
-
-
